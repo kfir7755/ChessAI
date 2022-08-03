410b52c55be09eda2c3d0f8f5b627a5896b13b49
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2022-08-03 21:48:13 +0300

    add

--- Constants.py
+++ Constants.py
@@ -1,2 +1,11 @@
+# constants files
 N = 8
-
+moves = 5
+green = (119, 149, 86)
+white = (235, 236, 208)
+king_eval = 5 * moves
+queen_eval = 10
+bishop_eval = 3
+rook_eval = 3
+knight_eval = 3
+pawn_eval = 1
